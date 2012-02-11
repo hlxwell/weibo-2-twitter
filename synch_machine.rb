9af87b739e192f16bd2847ed91b076d2dd453e57
Michael He <hlxwell@gmail.com>
2012-02-11 15:15:55 +0800

    add twitter post code

--- synch_machine.rb
+++ synch_machine.rb
@@ -3,8 +3,12 @@ Bundler.setup
 %w(eventmachine oauth json weibo).each { |dependency| require dependency }
 
 require File.join(".", "lib", "weibo_syncher")
+require File.join(".", "lib", "twitter_syncher")
 
 EM.run do
   weibo = WeiboSyncher.new
   weibo.update_status "test"
+
+  twitter = TwitterSyncher.new
+  twitter.update_status "test"
 end
