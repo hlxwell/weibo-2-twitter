9eb0087906beb2e20ec8dc61668d19af87f8e881
Michael He <hlxwell@gmail.com>
2012-02-12 15:58:48 +0800

    fix test

--- spec/weibo_syncher_spec.rb
+++ spec/weibo_syncher_spec.rb
@@ -3,7 +3,7 @@ require "./lib/weibo_syncher"
 describe 'Weibo syncher' do
   it "should be able to load timeline" do
     weibo = WeiboSyncher.new
-    updates = weibo.get_user_timeline
+    updates = weibo.get_unsynched_updates
     updates.size.should > 0
   end
 
